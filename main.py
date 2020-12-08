15dae5a481bcdbba6021cf9f29fd948914e9ee41
David Hernandez <desdeelfrionorte@gmail.com>
2020-12-08 13:58:26 -0500

    try again

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    win = turtle.Screen()
+    fred = turtle.pen()
+
+    win.exitonclick()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#    turtle.mainloop()
